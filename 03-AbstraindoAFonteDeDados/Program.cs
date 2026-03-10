7f475cc57f4c73b8703a2eafb0ae619d3e57ad40
Gustavo H. Coelho <gustavo@coelho.inf.br>
2026-03-10 12:15:57 -0300

    aplicado a funcionalidade de filtragem por artista criando um método de extensão e encadeamento

--- 03-AbstraindoAFonteDeDados/Program.cs
+++ 03-AbstraindoAFonteDeDados/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+
+var musicasColdplay = 
+        ObterMusicas(stream).
+        FiltrarMusicasPor("Adele");
+
+ExibirMusicas(musicasColdplay);
+
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    System.Console.WriteLine("Exibindo a lista de músicas:");
+
+    var contador = 0;
+    foreach (var musica in musicas)
+    {
+        contador++;
+        if (contador > 10) break;
+
+        System.Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista})");
+        
+    }
+}
+
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine();
+    while (linha is not null)
+    {
+        var partes = linha.Split(';');
+        var musica = new Musica
+        {
+            Titulo = partes[0],
+            Artista = partes[1],
+            Duracao = int.Parse(partes[2])
+        };
+        yield return musica;
+        linha = stream.ReadLine();
+    }
+}
+
+static class MusicasExtensions
+{
+    public static IEnumerable<Musica> FiltrarMusicasPor(this IEnumerable<Musica> musicas, string artista)
+    {
+        foreach (var musica in musicas)
+            if (musica.Artista == artista) yield return musica;
+    }
+}
+
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+}
