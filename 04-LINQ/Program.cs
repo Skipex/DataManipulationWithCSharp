8a2e359859ceef1acc153a58fdc6ff61049684e2
Gustavo H. Coelho <gustavo@coelho.inf.br>
2026-03-10 16:18:42 -0300

    arquivos iniciais da aula 4

---
+++ 04-LINQ/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+
+var musicas = 
+        ObterMusicas(stream) 
+        .Where( m => m.Artista == "Metallica")
+        .Where( m => m.Duracao >= 400);
+
+
+ExibirMusicas(musicas);
+
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    System.Console.WriteLine("Exibindo a lista de músicas:");
+
+    var contador = 0;
+    foreach (var musica in musicas)
+    {
+        contador++;
+        if (contador > 10) break;
+
+        System.Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista}) - {musica.Duracao} segundos");
+        
+    }
+}
+
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine();
+    while (linha is not null)
+    {
+        var partes = linha.Split(';');
+        var musica = new Musica
+        {
+            Titulo = partes[0],
+            Artista = partes[1],
+            Duracao = int.Parse(partes[2])
+        };
+        yield return musica;
+        linha = stream.ReadLine();
+    }
+}
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+}
