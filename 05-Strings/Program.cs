31a0dc0f6fb4e333956499d21dc8206e0381f628
Gustavo H. Coelho <gustavo@coelho.inf.br>
2026-03-24 16:22:42 -0300

    criada validação de senha e utilizada algumas funções de string

--- 05-Strings/Program.cs
+++ 05-Strings/Program.cs
@@ -1,6 +1,22 @@
 ﻿using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
 using var stream = new StreamReader(arquivo);
 
+//var musicas = ObterMusicas(stream)
+//                .Where(m => m.Titulo.StartsWith('T'))
+//                .Take(50);
+//ExibirMusicas(musicas);
+
+var musica = ObterMusicas(stream)
+                .Where(m => m.Titulo.StartsWith('T'))
+                .FirstOrDefault();
+
+if(musica is not null)
+{
+    System.Console.WriteLine($"A música é: {musica.Titulo} - {musica.Artista}");
+    musica.Titulo = musica.Titulo.Replace("The ", "");
+    System.Console.WriteLine($"A música é: {musica.Titulo} - {musica.Artista}");
+}
+
 
 
 void ExibirMusicas(IEnumerable<Musica> musicas)
@@ -13,7 +29,7 @@ void ExibirMusicas(IEnumerable<Musica> musicas)
         contador++;
         if (contador > 10) break;
 
-        System.Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista}) - {musica.Duracao} segundos");
+        System.Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista}) - {musica.Duracao}s [{musica.Lancamento:dd/MM/yyyy}]");
         
     }
 }
@@ -29,17 +45,39 @@ IEnumerable<Musica> ObterMusicas(StreamReader stream)
             Titulo = partes[0],
             Artista = partes[1],
             Duracao = int.Parse(partes[2]),
-            Generos = partes[3].Split(',').Select(g => g.Trim())
+            Generos = partes[3].Split(',', StringSplitOptions.TrimEntries),
+            Lancamento = Convert.ToDateTime(partes[4])
         };
         yield return musica;
         linha = stream.ReadLine();
     }
 }
 
+void ValidaSenha(string senha = "123456!sA")
+{
+    var totalCaracteres = senha.Length;
+    var totalMaiusculas = senha.Count(c => char.IsUpper(c));
+    var totalMinusculas = senha.Count(c => char.IsLower(c));
+    var totalDigitos = senha.Count(c => char.IsDigit(c));
+    var totalSimbolos = senha.Count(c => !char.IsLetterOrDigit(c));
+
+    if ( totalCaracteres < 8 ||
+            totalMaiusculas == 0 ||
+            totalMinusculas == 0 ||
+            totalDigitos == 0 ||
+            totalSimbolos == 0 )
+    {
+        System.Console.WriteLine("A senha é fraca.");
+    } else
+    {
+        System.Console.WriteLine("A senha é forte.");
+    }
+}
 class Musica
 {
     public string Titulo { get; set; }
     public string Artista { get; set; }
     public int Duracao { get; set; }
     public IEnumerable<string> Generos { get; set; }
+    public DateTime Lancamento { get; set; }
 }
