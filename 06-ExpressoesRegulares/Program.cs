ec8dcd642cd75a8c66fda1c92bcb52e5bdca85e4
Gustavo H. Coelho <gustavo@coelho.inf.br>
2026-03-25 16:52:10 -0300

    ajustados os arquivos iniciais do projeto da aula 6

--- 06-ExpressoesRegulares/Program.cs
+++ 06-ExpressoesRegulares/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+var musicas = ObterMusicas(stream)
+                .Take(20);
+ExibirMusicasEmTabela(musicas);
+
+void ExibirMusicasEmTabela(IEnumerable<Musica> musicas)
+{
+    System.Console.WriteLine("Exibindo a lista de músicas:");
+
+    var colunaTitulo = "Título".PadRight(40);
+    var colunaArtista = "Artista".PadRight(35);
+    var colunaDuracao = "Duração".PadRight(10);
+    var colunaLancamento = "Lançada em".PadRight(12);
+
+    System.Console.WriteLine($"{colunaTitulo} | {colunaArtista} | {colunaDuracao} | {colunaLancamento}");
+    System.Console.WriteLine("".PadLeft(100, '='));
+
+
+    var contador = 0;
+    foreach (var musica in musicas)
+    {
+        var duracao = string.Format("{0, -11:F3}", musica.Duracao/60.0);
+        System.Console.WriteLine($"{musica.Titulo, -41} {musica.Artista, -36}  {duracao} {musica.Lancamento, -16: dd/MM/yyyy}");
+        
+    }
+}
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    System.Console.WriteLine("Exibindo a lista de músicas:");
+
+    var contador = 0;
+    foreach (var musica in musicas)
+    {
+        contador++;
+        if (contador > 10) break;
+
+        System.Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista}) - {musica.Duracao}s [{musica.Lancamento:dd/MM/yyyy}]");
+        
+    }
+}
+
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine();
+    while (linha is not null)
+    {
+        var partes = linha.Split(';');
+
+        if(partes.Length == 5)
+        {
+            var musica = new Musica
+            {
+                Titulo = string.IsNullOrWhiteSpace(partes[0]) ? "Título não definido" : partes[0],
+                Artista = string.IsNullOrWhiteSpace(partes[1]) ? "Artista não definido" : partes[1],
+                Duracao = int.TryParse(partes[2], out int duracao) ? duracao : 0,
+                Generos = partes[3].Split(',', StringSplitOptions.TrimEntries),
+                Lancamento = DateTime.TryParse(partes[4], out var data) ? data : DateTime.Today
+            };
+            yield return musica;
+        }
+        linha = stream.ReadLine();
+    }
+}
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public IEnumerable<string> Generos { get; set; }
+    public DateTime Lancamento { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Titulo} ({Artista}) - {Duracao}s [{Lancamento:dd/MM/yyyy}]";
+    }
+}
