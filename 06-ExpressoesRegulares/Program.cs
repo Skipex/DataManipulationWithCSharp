e3e75f8e8d4b235890326ed189ffc2369908174d
Gustavo H. Coelho <gustavo@kobit.com.br>
2026-05-05 08:43:59 -0300

    Abtido a duração das músicas através da captura com expressão regular

--- 06-ExpressoesRegulares/Program.cs
+++ 06-ExpressoesRegulares/Program.cs
@@ -1,23 +1,11 @@
 ﻿using System.Text.RegularExpressions;
-
 using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
 using var stream = new StreamReader(arquivo);
 
-var linha = "The Broken Road;Rolling Stones;6:39;Rock, Blues Rock;13/09/1974";
-var match = Regex.Match(linha, @"\d:\d\d");
-if (match.Success)
-{
-    System.Console.WriteLine("Duração encontrada: " + match.Value);
-} else
-{
-    System.Console.WriteLine("Duração não encontrada.");
-}
-
-/*
 var musicas = ObterMusicas(stream)
                 .Take(20);
 ExibirMusicasEmTabela(musicas);
-*/
+
 void ExibirMusicasEmTabela(IEnumerable<Musica> musicas)
 {
     System.Console.WriteLine("Exibindo a lista de músicas:");
@@ -34,9 +22,9 @@ void ExibirMusicasEmTabela(IEnumerable<Musica> musicas)
     var contador = 0;
     foreach (var musica in musicas)
     {
-        var duracao = string.Format("{0, -11:F3}", musica.Duracao/60.0);
-        System.Console.WriteLine($"{musica.Titulo, -41} {musica.Artista, -36}  {duracao} {musica.Lancamento, -16: dd/MM/yyyy}");
-        
+        var duracao = string.Format("{0, -11:F3}", musica.Duracao / 60.0);
+        System.Console.WriteLine($"{musica.Titulo,-41} {musica.Artista,-36}  {duracao} {musica.Lancamento,-16: dd/MM/yyyy}");
+
     }
 }
 
@@ -51,24 +39,34 @@ void ExibirMusicas(IEnumerable<Musica> musicas)
         if (contador > 10) break;
 
         System.Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista}) - {musica.Duracao}s [{musica.Lancamento:dd/MM/yyyy}]");
-        
+
     }
 }
 
 IEnumerable<Musica> ObterMusicas(StreamReader stream)
 {
     var linha = stream.ReadLine();
+    int duracao = 0;
+
     while (linha is not null)
     {
+        var match = Regex.Match(linha, @"(\d?\d):(\d\d)");
+        if (match.Success)
+        {
+            var minutos = int.Parse(match.Groups[1].Value);
+            var segundos = int.Parse(match.Groups[2].Value);
+            duracao = (minutos * 60) + segundos;
+        }
+
         var partes = linha.Split(';');
 
-        if(partes.Length == 5)
+        if (partes.Length == 5)
         {
             var musica = new Musica
             {
                 Titulo = string.IsNullOrWhiteSpace(partes[0]) ? "Título não definido" : partes[0],
                 Artista = string.IsNullOrWhiteSpace(partes[1]) ? "Artista não definido" : partes[1],
-                Duracao = int.TryParse(partes[2], out int duracao) ? duracao : 0,
+                Duracao = duracao,
                 Generos = partes[3].Split(',', StringSplitOptions.TrimEntries),
                 Lancamento = DateTime.TryParse(partes[4], out var data) ? data : DateTime.Today
             };
